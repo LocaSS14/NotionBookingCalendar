7ac73cc0db9f4f64221acb0a68f7256e46dc1581
LocaSS14 <artofcamdentattooshop@gmail.com>
2025-02-24 22:51:01 +0000

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 // Replace with your actual Netlify function endpoint
 // e.g. "https://your-site.netlify.app/.netlify/functions/book-appointment"
-const NETLIFY_FUNCTION_URL = "https://app.netlify.com/sites/kaleidoscopic-valkyrie-450c6a";
+const NETLIFY_FUNCTION_URL = "https://notion-booking-app.netlify.app//functions/book-appointment";
 
 document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("booking-form");
